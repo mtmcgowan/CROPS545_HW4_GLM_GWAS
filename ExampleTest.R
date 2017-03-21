2fd1eb4a7b535177a16dd99db20a959211bdd7e3
mtmcgowan <mmcgowan280@gmail.com>
2017-03-21 14:31:50 -0700

    Update ExampleTest.R

--- ExampleTest.R
+++ ExampleTest.R
@@ -1,7 +1,7 @@
 # 
 
 mdpN_raw=read.table(file="http://zzlab.net/GAPIT/data/mdp_numeric.txt",head=T)
-mdpN <- mdp_numeric[,2:3094]
+mdpN <- mdpN_raw[,2:3094]
 row.names(mdpN) <- mdpN_raw[,1]
 
 mdpPheno_raw=read.table(file="http://zzlab.net/GAPIT/data/mdp_SNP_information.txt",head=T)
