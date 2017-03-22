6b61470ad29419355f2b6e698ab5a57047b7e61d
mtmcgowan <mmcgowan280@gmail.com>
2017-03-22 15:10:01 -0700

    Replaced the package folder
    Something was corrupted in the original folder

--- GWASbyGLMwPCA/R/plot_manhattan.R
+++ GWASbyGLMwPCA/R/plot_manhattan.R
@@ -1,7 +1,7 @@
 #' plot_manhattan
 #'
-#' This function takes the p-value output from GWASbyGLM and combines it with SNP positional information to generate a manhattan plot using the 'qqman' package
-#'
+#' This function performs a genome-wide-association study using the generalized linear model paradigm. Users have the option to include their own covariates, but can also perform principle component analysis.
+#' If so, the 'princomp' function is applied to the genotype matrix and components that explain more than 1% of the variance are included as long as they are not also correlated with user provided covariates.
 #' @param mdpSNPinfo a data.frame with 3 columns containing snp name, chromosome, and basepair position. SNPs must be in the same order as the mdpN genotype matrix used for GWASbyGLM
 #' @param GWASout a vector containing p-values generated with the GWASbyGLM function
 #' @param error a value from 0 to 1 indicating what type-1 error threshold should be used for calculating a Bonferroni cutoff. Default set to 0.05.
@@ -25,7 +25,7 @@ plot_manhattan <- function(mdpSNPinfo, GWASout, error = 0.05) {
   testct <- nrow(manhattan_frame)
 
   # Calculate the Bonferroni cutoff
-  bonferroni <- -log10(error/testct)
+  bonferroni <- -log(error/testct)
 
   # Generate the manhattan plot
   manhattan(manhattan_frame, suggestiveline = FALSE, genomewideline = bonferroni)
